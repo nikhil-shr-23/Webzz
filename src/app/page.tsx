3a697e28b8b3732b47a558e93ad190b07844efba
Nikhil Sharma <nikhilijs@gamil.com>
2025-07-18 11:49:33 +0530

    bgjobsinngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,30 @@
 
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Client } from "./Client";
-import { Suspense } from "react";
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 
 
+const Home =  () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess:()=>{
+      toast.success("Background Job Successfully Invoked")
+    }
+  }))
 
-
-
-
-
-const Home =  async () => {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.lol.queryOptions({text: "world"}))
  
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Client/>
-      </Suspense>
-    </HydrationBoundary>
+   <div className="p-4 max-w-7xl mx-auto">
+    
+    <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({text: "nikhilzi"})}>
+      Invoke Background Job
+    </Button>
+    
+   </div>
   );
 }
 
