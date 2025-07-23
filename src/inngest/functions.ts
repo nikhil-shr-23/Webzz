0e814d3d90e07220b467455e9ec1836e7aa009ff
Nikhil Sharma <nikhilijs@gamil.com>
2025-07-23 10:03:28 +0530

    done with sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,12 +1,20 @@
 import { inngest } from "./client";
-import { createAgent, Agent, openai } from '@inngest/agent-kit'
+import { createAgent,  openai } from '@inngest/agent-kit'
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event,step }) => {
+
+    const sandboxId = await step.run("get-sandvox-id", async()=>{
+      const sandbox = await Sandbox.create("webzzr-nextjs-2");
+      return sandbox.sandboxId;
+    })
     
+
     const codiza = createAgent({
       name: "writer",
       system: "You are an expert next.js developer and you write readable, maintainble code, you write simple nextjs snippets too and you are proficient with react.",
@@ -18,7 +26,13 @@ export const helloWorld = inngest.createFunction(
     );
     console.log(output);
 
+    const sandboxUrl = await step.run("get-sandvox-url", async()=>{
+      const sandbox = await getSandbox(sandboxId)
+       const host =  sandbox.getHost(3000);
+       return `https://${host}`;
+    })
 
-    return { output };
+
+    return { output, sandboxUrl };
   },
 ); 
